7a8b36a8f68007abf90c87bf34c42972712bf404
Justin Lee <ju3tinlee@gmail.com>
2025-12-13 23:23:32 -0800

    Loading animation for plots

---
+++ main.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function() {
+        const plotContainers = document.querySelectorAll('.plot-container');
+        plotContainers.forEach(container => {
+            const iframe = container.querySelector('iframe');
+            
+            if (iframe) {
+                const loader = document.createElement('div');
+                loader.className = 'loader';
+                container.insertBefore(loader, iframe);
+                iframe.onload = function() {
+                    loader.style.display = 'none';
+                    iframe.classList.add('loaded');
+                };
+            }
+        });
+    });
